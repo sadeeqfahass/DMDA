31e4f20033b7f4f51647ea933856b7999ba7d418
sadeeq fahass <sadeeqfhassan@gmail.com>
2023-07-24 12:10:04 +0100

    .

--- src/components/Soon.jsx
+++ src/components/Soon.jsx
@@ -3,13 +3,12 @@ import React from "react";
 function Soon() {
   return (
     <>
-      <div className="text-con">
-        <h1>Coming Soon!</h1>
-        <h3>Dawanau International Commodity Market...</h3>
+      <div className="container">
+        <div className="text-con">
+          <h1>Coming Soon!</h1>
+          <h3>Dawanau International Commodity Market...</h3>
+        </div>
       </div>
-      <img src="cotton.jpg" alt="soy beans image" className="soybean"/>
-      <img src="soybeans.jpg" alt="soy beans image" className="soybean1"/>
-      {/* <img src="soybeans.jpg" alt="soy beans image" className="soybean2"/> */}
     </>
   );
 }
